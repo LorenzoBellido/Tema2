366c55086878ec84e6b8532bb64528611098a226
ljbellido <ljbellido@106-07.info2>
2023-10-03 13:35:09 +0200

    Tema 2

---
+++ Ejercicios_Tema2/src/ejerciciost2/Ejercicio1.java
@@ -0,0 +1,31 @@
+package ejerciciost2;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// Variable donde guardar el número
+		int numero;
+		
+		// Scanner para leer el teclado
+		Scanner sc = new Scanner(System.in);
+		
+		// Pedir un número al usuario
+		System.out.println("Introduzca un número: ");
+		// Leemos el número del teclado
+		numero = sc.nextInt();
+		
+		// Comprobamos si es par o impar con la condicional if y ejecutamos unas instrucciones segun el resultado
+		if (numero%2 == 0) {
+			System.out.println("El número es par.");
+		}
+		else {System.out.println("El número es impar.");
+		
+		// Cerramos el Scanner
+		sc.close();
+		}
+
+	}
+
+}
---
+++ Ejercicios_Tema2/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Ejercicios_Tema2 {
+}
