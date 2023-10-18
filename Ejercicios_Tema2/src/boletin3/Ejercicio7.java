77c423c315d89555adae12bd9da4ccd06656ad5d
LorenzoBellido <lorenzo.bellido@iesnervion.es>
2023-10-18 10:22:08 +0200

    t

--- Ejercicios_Tema2/src/boletin3/Ejercicio7.java
+++ Ejercicios_Tema2/src/boletin3/Ejercicio7.java
@@ -20,10 +20,26 @@ public class Ejercicio7 {
 		System.out.println("Introduzca un número del 1 al 100");
 		numero = sc.nextInt();
 		
-		while (numero != random){
+		while (numero != random && numero < 0){
+			if(numero>random) {
+				System.out.println("El número es menor");
+				
+			}
+			else if (numero<random) {
+				System.out.println("El número es mayor");
+				
+			}
+			System.out.println("Introduzca un número del 1 al 100");
+			numero = sc.nextInt();
 			
 		}
-		System.out.println("Has ganado.");
+		if (numero < 0) {
+			System.out.println("Has perdido");
+		}
+		else {
+			System.out.println("Has ganado");
+		}
+		
 		
 		// Cierro el Scanner
 		sc.close();
