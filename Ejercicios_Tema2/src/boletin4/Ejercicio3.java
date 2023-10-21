3a0926ccb7295c6d43b3306d408d3c7a8801fe7e
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2023-10-21 13:43:06 +0200

    Tema 2

--- Ejercicios_Tema2/src/boletin4/Ejercicio3.java
+++ Ejercicios_Tema2/src/boletin4/Ejercicio3.java
@@ -5,18 +5,29 @@ import java.util.Scanner;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// Variable donde guardar un número de veces
-		int n;
-		// Variable donde guardar la suma de los numeros
-		int suma;
-		
-		// Scanner para leer del teclado
-		Scanner sc = new Scanner(System.in);
-		
-		
-		System.out.println("Introduzca un número");
-			n = sc.nextInt();
+		// Variable donde guardar el número del usuario
+    	int n;
+    // Variable donde guardar el primer número de la suma
+    	int numero = 1;
+    // Variable donde guardar el total
+    	int total = 0;
+    // Scanner para leer del teclado
+    	Scanner sc = new Scanner(System.in);
+    	
+    // Pido al usuario un número
+    	System.out.print("Introduce un número: ");
+        n = sc.nextInt();
+    // Realizo el bucle donde se sumará a número el total y numero valdrá 1 más
+        do {
+        	total += numero;
+        	numero++;
+        } while (numero <= n);
+        
+    // Muestro el resultado
+        System.out.println("La suma de los números desde 1 hasta " + n + " es: " + total);
 
+    // Cierro el Scanner
+    sc.close();
 	}
 
 }
